66827e73fb725b77a9604f40d33797d09a628769
Abeer Iqbal <abeeriqbal@Awais-MacBook.local>
2017-09-20 09:48:56 +0500

    Log Message

--- GitUseExample/ViewController.swift
+++ GitUseExample/ViewController.swift
@@ -20,6 +20,13 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func buttonClicked(_ sender: UIButton) {
+        print("This is a Git tutorial")
+    }
+    
+    
+    
+    
 
 }
 
